d5e069cf9d0c85c772a7c223c22a3ecbb0ad4e07
Daniel Natale <daniel.natale@vendr.com>
2025-08-22 19:12:38 -0300

    do not depend on package json

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { Context } from "./context";
-import Package from "../package.json";
 import * as Tools from "./tools";
 
 export function makeServer(context: Context) {
   const server = new McpServer({
-    name: Package.name,
-    version: Package.version,
+    name: "vendr-mcp",
+    version: "1.0.0",
   });
 
   Tools.register(server, context);
